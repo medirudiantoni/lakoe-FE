625fd0b66637db5ae2a969cc027100fd47031e09
medi_rudiant <medirudiantoni2@gmail.com>
2025-01-21 21:27:51 +0700

    chore: tes husky

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -7,6 +7,7 @@ const Layout = () => {
             <LeftSide />
             <HStack w="100vw" minH="100vh" gap={0}>
                 <Box w="400px" h="100vh"></Box>
+                <Box position="fixed"></Box> // tes aja
                 <Box flex={1} w="100%" h="100vh" borderLeftWidth={1} borderColor="teal.400"></Box>
             </HStack>
             {/* <Box w="100vw" minH="100vh" display="grid" gridTemplateColumns="repeat(4, 1fr)" gap={0}>
