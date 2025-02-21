e26a1e92c6d7d20babdcffc68477f492182d44b2
medi_rudiant <medirudiantoni2@gmail.com>
2025-02-21 07:05:00 +0700

    fix: footer store.name

--- src/page/seller/components/footer.tsx
+++ src/page/seller/components/footer.tsx
@@ -23,7 +23,7 @@ export default function SellerFooter() {
         >
           <Box w="fit">
             <Box mb="10">
-              <SellerLogo brandName={store!.name} fontSize="2xl" />
+              <SellerLogo brandName={String(store?.name)} fontSize="2xl" />
             </Box>
             <Box mb="4"  color={'gray.700'}>
               <Heading fontWeight="semibold" display={'flex'} alignItems={'center'} gap={2} mb={'2'}>Kontak <span><Mail/></span></Heading>
